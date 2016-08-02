03a582dbfca5ecf66c70fec05994eae6d3254d81
manoel.ribeiro <manoel.ribeiro@LSBD-22006>
2016-08-02 15:48:11 -0300

    TGreenWave

--- TGreeWave/Program.cs
+++ TGreeWave/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using NDesk.Options;
+z
+using Microsoft.SolverFoundation.Common;
+using Microsoft.SolverFoundation.Solvers;
 
 namespace LK.TGreeWave
 {
@@ -7,51 +10,15 @@ namespace LK.TGreeWave
     {
         static void Main(string[] args)
         {
-            string osmPath = "";
-            int eps = -1;
-            int minTraffic = -1;
-            bool showHelp = false;
+            SimplexSolver solver = new SimplexSolver();
 
-            OptionSet parameters = new OptionSet() {
-                { "osm=", "path to the map file",                                                    v => osmPath = v},
-                { "eps=",   "size of the eps-neighborhood to be considered (integer)",               v => eps = Convert.ToInt32(v)},
-                { "minTraffic=", "minimum traffic considered (integer)",                             v => minTraffic = Convert.ToInt32(v)},
-                { "h|?|help",                                                                        v => showHelp = v != null},
-            };
+            int savid, vzvid;
+            solver.AddVariable("Saudi Arabia", out savid);
+            solver.SetBounds(savid, 0, 9000);
+            solver.AddVariable("Venezuela", out vzvid);
+            solver.SetBounds(vzvid, 0, 6000);
 
-            try
-            {
-                parameters.Parse(args);
-            }
-            catch (OptionException e)
-            {
-                Console.Write("TGreenWave: ");
-                Console.WriteLine(e.Message);
-                Console.WriteLine("Try `TGreenWave --help' for more information.");
-                return;
-            }
-
-
-            if (showHelp || string.IsNullOrEmpty(osmPath) || eps < 0 || minTraffic < 0)
-            {
-                ShowHelp(parameters);
-                return;
-            }
-            
         }
-
-        /// <summary>
-        /// Prints a help message
-        /// </summary>
-        /// <param name="p">The parameters accepted by this program</param>
-        static void ShowHelp(OptionSet p)
-        {
-            Console.WriteLine("Usage: TGreenWave [OPTIONS]+");
-            Console.WriteLine("Outputs hot routes found");
-            Console.WriteLine();
-
-            Console.WriteLine("Options:");
-            p.WriteOptionDescriptions(Console.Out);
-        }
+        
     }
 }
